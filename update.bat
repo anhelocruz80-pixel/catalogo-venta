8f3b3f82bb4bf4bc605fe70af44de6b8c198332e
TSOFT\michel.cruz <michel.cruz@tsoftlatam.com>
2025-12-29 20:33:51 -0300

    Actualizacion integracion bd posgertsql v56

--- update.bat
+++ update.bat
@@ -3,7 +3,7 @@ REM Script para actualizar el repo en GitHub Pages
 REM Uso: update.bat "mensaje de commit"
 
 SET MSG=%1
-IF "%MSG%"=="" SET MSG=Actualizacion integracion bd posgertsql v55
+IF "%MSG%"=="" SET MSG=Actualizacion integracion bd posgertsql v56
 
 echo 📦 Agregando cambios...
 git add .
