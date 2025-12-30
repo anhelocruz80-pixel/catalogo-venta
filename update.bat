a208b1107d86ca662179de6d5250ff28d5a37dea
TSOFT\michel.cruz <michel.cruz@tsoftlatam.com>
2025-12-30 13:39:05 -0300

    Actualizacion version profesional v2

--- update.bat
+++ update.bat
@@ -3,7 +3,7 @@ REM Script para actualizar el repo en GitHub Pages
 REM Uso: update.bat "mensaje de commit"
 
 SET MSG=%1
-IF "%MSG%"=="" SET MSG=Actualizacion version profesional v1
+IF "%MSG%"=="" SET MSG=Actualizacion version profesional v2
 
 echo 📦 Agregando cambios...
 git add .
