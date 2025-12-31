04fe8e34a84da221ff53e8cfd4351835a8da0132
TSOFT\michel.cruz <michel.cruz@tsoftlatam.com>
2025-12-31 09:57:02 -0300

    Actualizacion version profesional v19

--- commit.js
+++ commit.js
@@ -12,10 +12,19 @@ if (status === "AUTHORIZED") {
       <a href="index.html">Volver a la tienda</a>
     </div>
   `;
+} else if (status === "ABORTED") {
+  cont.innerHTML = `
+    <div class="card">
+      <h2>⚠️ Compra cancelada</h2>
+      <p>No se realizó ningún cargo.</p>
+      <a href="index.html">Volver a la tienda</a>
+    </div>
+  `;
 } else {
   cont.innerHTML = `
     <div class="card">
       <h2>❌ Pago rechazado</h2>
+      <p>El banco no autorizó el pago.</p>
       <a href="index.html">Volver</a>
     </div>
   `;
